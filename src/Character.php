0e9a73744ff060eb055d400aef7fbb4f7c488e61
FacuRamallo <framallo.ing@gmail.com>
2021-11-17 00:11:03 +0100

    iteration 3 done

--- src/Character.php
+++ src/Character.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-
+use phpDocumentor\Reflection\Types\Callable_;
 
 class Character
 {
@@ -12,10 +12,13 @@ class Character
     protected bool $alive = true;
     protected int $maxRange=0;
     protected array $factions=array(0=>0,1=>0,2=>0,3=>0);
-
-      
+    
+    
     public function hit(int $hitPoints, Character $target,$distanceToTarget) {
-        if(($this->charId !== $target->charId)&&($this->isTargetInRange($distanceToTarget))){
+        if($this->checkCharsAreAllies($target)){
+            return;
+        }
+        if(($this->getCharId() !== $target->getCharId())&&($this->isTargetInRange($distanceToTarget))){
             $targetLifePoints = $target->getHealth(); 
             $hitPoints= $this->compareLevels($hitPoints,$target);
             $targetLifePoints -= $hitPoints; 
@@ -24,6 +27,27 @@ class Character
         }
     }
     
+    public function heal(int $pointsOfHeal,$damaged=null) {
+        if ($damaged==null){
+            if($this->alive){ 
+                $healedLifePoints = $this->getHealth();
+                $healedLifePoints += $pointsOfHeal; 
+                $this-> setHealth($healedLifePoints);
+                $this-> checkHealNotOver1000();
+                }
+            return;
+        }
+
+        if($this->checkCharsAreAllies($damaged)){
+            if($damaged->alive){ 
+                $healedLifePoints = $damaged->getHealth();
+                $healedLifePoints += $pointsOfHeal; 
+                $damaged-> setHealth($healedLifePoints);
+                $damaged-> checkHealNotOver1000();
+                }
+        }
+        
+    }
     
     public function setHealth(int $lifePoints) {
         $this->health = $lifePoints;
@@ -40,15 +64,20 @@ class Character
     public function joinFaction($faction,$gameFactions){
         $factionIndex = array_search($faction,$gameFactions);
         $this-> factions[$factionIndex] = 1;
-        
     }
+
+    public function leaveFaction($faction,$gameFactions){
+        $factionIndex = array_search($faction,$gameFactions);
+        $this-> factions[$factionIndex] = 0;
+    }
+
     public function getHealth()
     {
        return $this->health;
     }
 
-   public function getLevel()
-   {
+    public function getLevel()
+    {
        return $this->level;
     }
 
@@ -58,6 +87,10 @@ class Character
 
     public function getFactions(){
         return $this->factions;
+    }
+
+    public function getCharId(){
+        return $this->charId;
     }
     
     public function die() {
@@ -81,16 +114,6 @@ class Character
         }
     }
 
-    public function heal(int $pointsOfHeal) {
-        
-        if($this->alive){ 
-        $healedLifePoints = $this->getHealth();
-        $healedLifePoints += $pointsOfHeal; 
-        $this-> setHealth($healedLifePoints);
-        $this-> checkHealNotOver1000();
-        }
-        
-    }
 
     public function compareLevels($hitPoints, $target){
         $targetLevel = $target ->getLevel();
@@ -99,7 +122,7 @@ class Character
         if(($levelDelta >= 5)and($targetLevel > $attackerLevel)){
             $hitPoints = $hitPoints/2;
             return $hitPoints;
-        }elseif (($levelDelta >= 5)and($attackerLevel > $targetLevel)) {
+        }elseif (($levelDelta >= 5)and($targetLevel < $attackerLevel)) {
             $hitPoints = $hitPoints*1.5;
             return $hitPoints;
         }
@@ -113,7 +136,27 @@ class Character
         }
         return true;
     }
+    
+    public function factionsCharBelongsTo(){
+        
+        $numOfFactions = array_reduce
+        (
+            $this->factions,
+            function($carry,$item){
+                $carry += $item;
+                return $carry;
+            }
+        );
+        return $numOfFactions;
+    }
 
+    public function checkCharsAreAllies(Character $characterX) {
+        $intersectArray = array_diff_assoc($this->getFactions(),$characterX->getFactions());
+        if (count($intersectArray)==0 && $this->factionsCharBelongsTo()!=0){
+            return true;
+        }
+        return false;
+    }
     
 
 }
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -34,9 +34,9 @@ class CharacterTest extends TestCase
        
     public function test_character_can_damage_and_substract_from_health() {
 
-        $attaker = new Character();
-        $damaged = new Character();
-        $damaged ->setCharId();
+        $attaker = new MeleChar();
+        $damaged = new MeleChar();
+        
 
         $attaker->hit(100, $damaged,0);
 
@@ -147,16 +147,115 @@ class CharacterTest extends TestCase
 
         $Character1 -> joinFaction('faction_c',$gameFactions);
         $Character2 -> joinFaction('faction_b',$gameFactions);
+        $Character2 -> joinFaction('faction_a',$gameFactions);
 
         $Character1_factions = $Character1->getFactions();
         $Character2_factions = $Character2->getFactions();
         
         $this-> assertEquals(1, $Character1_factions[2]);
         $this-> assertEquals(1, $Character2_factions[1]);
-        
+        $this-> assertEquals(1, $Character2_factions[0]);
 
     }
 
+    public function test_Newly_created_Characters_belong_to_no_Faction(){
+        $Character1 = new MeleChar;
+
+        $numOfFactionsCharBelongsTo = $Character1->factionsCharBelongsTo();
+
+        $this-> assertEquals(0,$numOfFactionsCharBelongsTo);
+    }
+
+    public function test_A_Character_may_Join_or_Leave_one_or_more_Factions(){
+        $Character1 = new MeleChar;
+       
+
+        $gameFactions=array(
+            0 => 'faction_a',
+            1 => 'faction_b',
+            2 => 'faction_c',
+            3 => 'faction_d'
+        );
+
+        $Character1 -> joinFaction('faction_a',$gameFactions);
+        $Character1 -> joinFaction('faction_c',$gameFactions);
+        $Character1 -> joinFaction('faction_d',$gameFactions);
+       
+
+        $Character1_factions = $Character1->getFactions();
+       
+        $this-> assertEquals(1, $Character1_factions[0]);
+        $this-> assertEquals(1, $Character1_factions[2]);
+        $this-> assertEquals(1, $Character1_factions[3]);
+
+        $Character1 -> leaveFaction('faction_a',$gameFactions);
+        $Character1 -> leaveFaction('faction_c',$gameFactions);
+        
+        $Character1_factions = $Character1->getFactions();
+
+        $this-> assertEquals(0, $Character1_factions[0]);
+        $this-> assertEquals(0, $Character1_factions[2]);
+        $this-> assertEquals(1, $Character1_factions[3]);
+    }
+
+    public function test_Players_belonging_to_the_same_Faction_are_considered_Allies() {
+        $Character1 = new MeleChar;
+        $Character2 = new RangedChar;
+
+        $gameFactions=array(
+            0 => 'faction_a',
+            1 => 'faction_b',
+            2 => 'faction_c',
+            3 => 'faction_d'
+        );
+
+        $Character1 -> joinFaction('faction_a',$gameFactions);
+        $Character2 -> joinFaction('faction_a',$gameFactions);
+
+        $areCharactersAllies = $Character1 -> checkCharsAreAllies($Character2);
+
+        $this-> assertTrue($areCharactersAllies);
+    }
+
+    public function test_Allies_cannot_Deal_Damage_to_one_another() {
+        $attackerChar= new MeleChar;
+        $targetChar= new RangedChar;
+
+        $gameFactions=array(
+            0 => 'faction_a',
+            1 => 'faction_b',
+            2 => 'faction_c',
+            3 => 'faction_d'
+        );
+
+        $attackerChar -> joinFaction('faction_a',$gameFactions);
+        $targetChar -> joinFaction('faction_a',$gameFactions);
+
+        $attackerChar -> hit(100,$targetChar,0);
+
+        $this -> assertEquals(1000,$targetChar->getHealth());
+    }
+
+    public function test_Allies_can_Heal_one_another(){
+        $healerChar= new MeleChar;
+        $damagedChar= new RangedChar;
+
+        $gameFactions=array(
+            0 => 'faction_a',
+            1 => 'faction_b',
+            2 => 'faction_c',
+            3 => 'faction_d'
+        );
+
+        $damagedChar -> setHealth(600);
+
+        $healerChar -> joinFaction('faction_a',$gameFactions);
+        $damagedChar -> joinFaction('faction_a',$gameFactions);
+
+        $healerChar -> heal(200,$damagedChar);
+
+        $this -> assertEquals(800,$damagedChar->getHealth());
+    }
    
     
 }
